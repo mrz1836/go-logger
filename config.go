6e313a62ecefc967006bdfc0caac2b772cf965c4
mrz1836 <mrz1818@gmail.com>
2020-11-29 11:35:22 -0500

    Fix for tests

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ import "time"
 // Package constants
 const (
 	LogEntriesPort = "10000"
-	LogEntriesURL  = "data.logentries.com" // "us.data.logs.insight.rapid7.com"
+	LogEntriesURL  = "us.data.logs.insight.rapid7.com" // "us.data.logs.insight.rapid7.com"
 	MaxRetryDelay  = 2 * time.Minute
 	RetryDelay     = 100 * time.Millisecond
 )
