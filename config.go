c0c47709a8c240c9ef387236a3d1e1bcc3b4b228
mrz1836 <mrz1818@gmail.com>
2021-03-26 10:34:48 -0400

    Trying a different port (github actions issue)

--- config.go
+++ config.go
@@ -4,8 +4,8 @@ import "time"
 
 // Package constants
 const (
-	LogEntriesPort = "10000"                           // 80, 514, 10000
-	LogEntriesURL  = "us.data.logs.insight.rapid7.com" // "data.logentries.com"  "eu.data.logs.insight.rapid7.com"
+	LogEntriesPort = "443"                             // 80, 514, 443, 10000
+	LogEntriesURL  = "us.data.logs.insight.rapid7.com" // "data.logentries.com" "data.insight.rapid7.com (US)"  "eu.data.logs.insight.rapid7.com"
 	MaxRetryDelay  = 2 * time.Minute
 	RetryDelay     = 100 * time.Millisecond
 )
