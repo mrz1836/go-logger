92ce129481840d2d2dd73e03153c56120a50c52a
mrz1836 <mrz1818@gmail.com>
2021-03-26 10:12:29 -0400

    Trying different log entries endpoint

--- config.go
+++ config.go
@@ -4,8 +4,8 @@ import "time"
 
 // Package constants
 const (
-	LogEntriesPort = "10000"               // 80, 514, 10000
-	LogEntriesURL  = "data.logentries.com" // "us.data.logs.insight.rapid7.com"  "eu.data.logs.insight.rapid7.com"
+	LogEntriesPort = "10000"                           // 80, 514, 10000
+	LogEntriesURL  = "us.data.logs.insight.rapid7.com" // "data.logentries.com"  "eu.data.logs.insight.rapid7.com"
 	MaxRetryDelay  = 2 * time.Minute
 	RetryDelay     = 100 * time.Millisecond
 )
