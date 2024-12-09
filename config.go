c0eb4058a7298315a9f7abef82d719aaf386faaf
mrz1836 <mrz1818@gmail.com>
2024-12-09 13:48:46 -0500

    Updated default IP address

--- config.go
+++ config.go
@@ -7,7 +7,7 @@ import "time"
 // Package constants
 const (
 	LogEntriesPort         = "10000"               // 80, 514, 443, 10000
-	LogEntriesTestEndpoint = "34.253.67.177"       // This is an IP for now, since Github Actions fails on resolving the domains
+	LogEntriesTestEndpoint = "52.214.43.195"       // This is an IP for now, since GitHub Actions fails on resolving the domains
 	LogEntriesURL          = "data.logentries.com" // "data.insight.rapid7.com" "eu.data.logs.insight.rapid7.com"
 	MaxRetryDelay          = 2 * time.Minute
 	RetryDelay             = 100 * time.Millisecond
